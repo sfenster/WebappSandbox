d801225888cf9038bf17171784c7fa39900704a9
sfenster <scottfenstermaker.gmail.com>
2020-01-05 23:00:08 +0000

    removed threading code for pythonanywhere

--- FlaskTest/PythonMegaCourse/PortfolioSite.py
+++ FlaskTest/PythonMegaCourse/PortfolioSite.py
@@ -14,12 +14,11 @@ app = Flask(__name__, template_folder=template_dir) #templates not in root app d
 @app.route('/')
 def home():
     return render_template("home.html")
-  
-@app.route('/about')  
+
+@app.route('/about')
 def about():
     return "About Page content goes here!"
 
-  
-#Threading code below is not from the lesson, but necessary to run on colab
-import threading
-threading.Thread(target=app.run, kwargs={'host':'0.0.0.0','port':80}).start() 
+
+if __name__ == '__main__':
+    app.run()
